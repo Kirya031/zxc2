fe6683f454c405997f1ddcfa1d87efd360ec16bb
Kirill <petrishevk5@gmail.com>
2025-12-15 13:30:33 +0300

    first commit

---
+++ MySolutionNew/MyProject.Tests/Test1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyProject;
+
+public static class MathOperations
+{
+    public static double Add(double a, double b) => a + b;
+    
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Деление на ноль недопустимо.");
+        }
+        return a / b;
+    }
+}
+
+public static class StringOperations
+{
+    public static string ToLower(string s) => s?.ToLower() ?? "";
+    
+    public static int GetLength(string s) => s?.Length ?? 0;
+}
---
+++ MySolutionNew/MyProject/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace MyProject;
+
+public static class StringOperations
+{
+    public static string ToLower(string s) => s?.ToLower() ?? "";
+    
+    public static int GetLength(string s) => s?.Length ?? 0;
+}
